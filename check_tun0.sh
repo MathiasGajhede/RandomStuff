4a1896d0c0f0912d6ef69455129f347cac9658d2
Mathias Gajhede <mag@venturecapitals.dk>
2022-05-09 16:21:11 +0200

    Update check_tun0.sh

--- check_tun0.sh
+++ check_tun0.sh
@@ -4,9 +4,9 @@ for i in tun0
   do OUT="$(ip a show $i up)";
 
     if [[ $OUT == *"does not exist."* ]]; then
-      echo "$i is down"
-    else
-      echo "$i is up"    
+      /startup/run.sh &> /startup/feedback.txt &
+      cd /startup/
+      ./firewall-rules.sh &> /startup/firewall-rules.sh &
     fi
 
 done
