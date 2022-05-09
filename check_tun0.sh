33b4b64a14618d641498f4fe40237403f1a62977
Mathias Gajhede <mag@venturecapitals.dk>
2022-05-09 16:35:29 +0200

    Update check_tun0.sh

--- check_tun0.sh
+++ check_tun0.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ "`ifconfig | grep tun0`" != "" ] ; 
-	then 
-		echo yes; 
-	else 
-		/startup/run.sh &> /startup/feedback.txt &
-		cd /startup/
-		./firewall-rules.sh &> /startup/firewall-rules.sh &
-		echo no; 
-	fi
+then 
+	echo yes; 
+else 
+	/startup/run.sh &> /startup/feedback.txt &
+	cd /startup/
+	./firewall-rules.sh &> /startup/firewall-rules.sh &
+	echo no; 
+fi
