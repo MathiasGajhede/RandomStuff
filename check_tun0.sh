b9b370ab50fd488378193d2827749eebd96bae3d
Mathias Gajhede <mag@venturecapitals.dk>
2022-05-09 16:22:40 +0200

    Update check_tun0.sh

--- check_tun0.sh
+++ check_tun0.sh
@@ -1,12 +1,8 @@
-#!bin/bash
+#!/bin/bash
 
-for i in tun0
-  do OUT="$(ip a show $i up)";
-
-    if [[ $OUT == *"does not exist."* ]]; then
-      /startup/run.sh &> /startup/feedback.txt &
-      cd /startup/
-      ./firewall-rules.sh &> /startup/firewall-rules.sh &
-    fi
-
-done
+if [ "`ifconfig | grep tun0`" != "" ] ; 
+	then 
+		echo yes; 
+	else 
+		echo no; 
+	fi
